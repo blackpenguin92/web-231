0241681c389d5b9248f23e95780a6cbe8ff875d5
Cesar <cesaredwin2009@gmail.com>
2025-04-06 17:22:40 -0500

    STARTED

---
+++ week-3/project02-04/project02-04.js
@@ -0,0 +1,30 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-04
+
+      Application to calculate the cost of a restaurant order plus tax
+      Author: Cesar Acevedo
+      Date: 4/6/25
+
+      Filename: project02-04.js
+ */
+
+const CHICKEN_PRICE = 10.95;
+const HALIBUT_PRICE = 12.95;
+const BURGER_PRICE = 9.95;
+const SALMON_PRICE = 18.95;
+const SALAD_PRICE = 7.95;
+const SALES_TAX = 0.07;
+
+function calcTotal(){
+  let cost = 0;
+}
+
+
+
+
+
+// Function to display a numeric value as a text string in the format $##.##
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }

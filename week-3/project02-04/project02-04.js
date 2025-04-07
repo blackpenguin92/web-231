af3deec3e4dabffd9718e6651a83906df0b9f3bd
CesarEdwin <cesaredwin2009@gmail.com>
2025-04-06 21:55:18 -0500

    finish project02-04

--- week-3/project02-04/project02-04.js
+++ week-3/project02-04/project02-04.js
@@ -16,13 +16,42 @@ const SALMON_PRICE = 18.95;
 const SALAD_PRICE = 7.95;
 const SALES_TAX = 0.07;
 
-function calcTotal(){
+document.getElementById("chicken").addEventListener("click", calcTotal);
+document.getElementById("halibut").addEventListener("click", calcTotal);
+document.getElementById("burger").addEventListener("click", calcTotal);
+document.getElementById("salmon").addEventListener("click", calcTotal);
+document.getElementById("salad").addEventListener("click", calcTotal);
+
+function calcTotal() {
+
+  // initialize variables for food items & cost
   let cost = 0;
+  let buyChicken = document.getElementById("chicken").checked;
+  let buyHalibut = document.getElementById("halibut").checked;
+  let buyBurger = document.getElementById("burger").checked;
+  let buySalmon = document.getElementById("salmon").checked;
+  let buySalad = document.getElementById("salad").checked;
+
+  // add cost of item to cost if selected
+  cost += buyChicken ? CHICKEN_PRICE : 0;
+  cost += buyHalibut ? HALIBUT_PRICE : 0;
+  cost += buyBurger ? HALIBUT_PRICE : 0;
+  cost += buySalmon ? SALMON_PRICE : 0;
+  cost += buySalad ? SALAD_PRICE : 0;
+
+  // show current Total w/o taxes
+  document.getElementById("foodTotal").innerHTML = formatCurrency(cost);
+
+  // calculate taxes
+  let tax = cost * SALES_TAX;
+
+  // show current taxes
+  document.getElementById("foodTax").innerHTML = formatCurrency(tax);
+
+  // calculate totalCost and show bill
+  let totalCost = cost + tax;
+  document.getElementById("totalBill").innerHTML = formatCurrency(totalCost);
 }
-
-
-
-
 
 // Function to display a numeric value as a text string in the format $##.##
  function formatCurrency(value) {
